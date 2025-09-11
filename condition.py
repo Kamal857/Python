84a5ec8cb99a17dd190116ac86b76ddbdcb8439f
Kamal Bohara <boharakamal857@gmail.com>
2025-09-11 19:40:06 +0545

    condition

---
+++ condition.py
@@ -0,0 +1,8 @@
+#to check the eligiblity of voting
+age=int(input("Enter your Age"))
+if(age>=18):
+    print("You can vote")
+elif(age<18):
+    print(" Sorry,You cannot Vote ")
+else:
+    print("End Of code")
