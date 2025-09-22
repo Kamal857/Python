a9b94f129c6982824eaf3ea143d79c7d0bdb22d4
Kamal Bohara <boharakamal857@gmail.com>
2025-09-22 20:11:56 +0545

    simple ineterst program

---
+++ simpleint.py
@@ -0,0 +1,6 @@
+p=float(input("Enter the Principal"))
+t=float(input("Enter the time: "))
+r=float(input("Enter the Rate of Principal: "))
+si=(p*t*r)/100
+print("The Intrest amount is :",si)
+print("The total amount is: ",si+p)
